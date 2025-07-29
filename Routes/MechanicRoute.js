078ce5aa72adc9fb577951a816889fb4cb689809
Muhammad Faraz       -    CS120212072 <cs120212072@kust.edu.pk>
2025-07-30 04:28:24 +0500

    Updated

--- Routes/MechanicRoute.js
+++ Routes/MechanicRoute.js
@@ -1,22 +1,16 @@
 
 const express = require('express');
 const router = express.Router();
-const {
-  addMechanic,
-  getAllMechanics,
-  getSingleMechanic,
-  updateMechanic,
-  deleteMechanic,
-} = require('../Controllers/MechanicController');
+const { addMechanic, getAllMechanics, getSingleMechanic, updateMechanic, deleteMechanic } = require('../Controllers/MechanicController');
 const auth = require ('../Middleware/authMiddleware');
 const { isAdmin } = require('../Controllers/AuthController');
 
 // Routes
-router.post('/add',  addMechanic);
-router.get('/', getAllMechanics);
-router.get('/:id', getSingleMechanic);
-router.put('/:id', updateMechanic);
-router.delete('/:id', deleteMechanic);
+router.post('/add', auth, isAdmin, addMechanic);
+router.get('/', auth, isAdmin, getAllMechanics);
+router.get('/:id', auth, isAdmin, getSingleMechanic);
+router.put('/:id', auth, isAdmin, updateMechanic);
+router.delete('/:id', auth, isAdmin, deleteMechanic);
 
 
 module.exports = router;
